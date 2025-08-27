a5e3cf4c0ca2bcf0d7d2ca63a2b83c9c5f813e97
Tupap1 <andresdavidcantillo@gmail.com>
2025-08-27 15:45:41 -0500

    partial configuration db

--- server/go.mod
+++ server/go.mod
@@ -1,4 +1,4 @@
-module github.com/Tupap1/Listedd
+module github.com/Tupap1/Listed/server
 
 go 1.24.5
 
--- server/main.go
+++ server/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+		"fmt"
 		"github.com/gofiber/fiber/v2"
 		"gorm.io/driver/mysql"
 		"gorm.io/gorm"
-		"os"
-		"github.com/tupap1/listed/server/models"
+		//"os"
+		"github.com/Tupap1/Listed/server/models"
 	)
 
 func main() {
-	dsn := os.Getenv("DB_DSN")
+	dsn := os.Getenv("root:7iu7Wi0@tcp(127.0.0.1:3306)/adm?charset=utf8mb4&parseTime=True&loc=Local")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect databaseeee:", err)
+	}
+
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		db.AutoMigrate(&models.User{})
-		return c.SendString("tablas creadas")
+		return c.SendString("tablas creadas porfavorsss")
 	})
 
 	app.Listen(":8080")
